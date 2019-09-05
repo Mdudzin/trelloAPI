4f3e67eb45754af6769f16ffaf6af3a4c9af287c
mdudzin <maciej.dudzin@gmail.com>
2019-09-05 12:10:02 +0200

    tests

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCardDto.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CreatedTrelloCard {
+public class CreatedTrelloCardDto {
 
     @JsonProperty("id")
     private String id;
@@ -15,7 +15,7 @@ public class CreatedTrelloCard {
     @JsonProperty("id")
     private String shortUrl;
 
-    public CreatedTrelloCard(String id, String name, String shortUrl) {
+    public CreatedTrelloCardDto(String id, String name, String shortUrl) {
         this.id = id;
         this.name = name;
         this.shortUrl = shortUrl;
